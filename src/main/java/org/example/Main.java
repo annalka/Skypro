f55fb0ae578e4ee28f73f19594418a2f5491f4fc
Комочек <asasinre@mail.ru>
2026-04-28 15:26:07 +0300

    homework_4

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,73 +2,116 @@ package org.example;
 
 public class Main {
     {
-        int age = 18;
-        if (age >=18){
-            System.out.println("ВЫ - совершеннолетний");
+        int clientOS = 0;
 
-        } else {
-            System.out.println("Вы - несовершеннолетний");
+        switch (clientOS) {
+            case 0:
+                System.out.println("Установите версию приложения для iOS по ссылке");
+                break;
+            case 1:
+                System.out.println("Установите версию приложения для Android по ссылке");
+                break;
         }
     }
-    {
-        int temperature = 7;
-        if (temperature >= 5){
-            System.out.println("Cегодня тепло, шапка не нужна");
-        }else {
-            System.out.println("Cегодня холодно, надень шапку");
-        }
-    }
-    {
-        int speed = 55;
-        if (speed <= 60){
-            System.out.println("Скорость в норме");
-        }else {
-            System.out.println("Cкорость превышена, вам штраф");
-    }
-    }
-    {
-        int age = 16;
-        if ((age >= 2) && (age<=6 )){
-            System.out.println("Вам следует пройти в детский сад");
-        } else if ((age>= 7) && (age <=17)) {
-            System.out.println("Вам в школу пройти");
-        } else if ((age>=18) && (age <=24)) {
-            System.out.println("Вам в ВУЗ надо");
-        } else if (age> 24) {
-            System.out.println("Вам на работу пора");
-        }
-    }
-    {
-        int age=8;
-        if (age <5){
-            System.out.println("Вам нельзя кататься на атракционе");
-        } else if ((age >=5)&&(age <=14)) {
-            System.out.println("Вам можно кататься только в споровождении взрослого");
-        }else{
-            System.out.println("Вам можно кататься на атракционе без взрослых");
-        }
-    }
-    {
-        int amount = 88;
-        if(amount <=60){
-            System.out.println("В вагоне есть сидячие места");
-        } else if ((amount>60)&&(amount<=102)) {
-            System.out.println("В вагоне есть стоячие места");
-        }else{
-            System.out.println("В вагоне нет мест");
-        }
-    }
-    {
-        int one=2;
-        int two=3;
-        int three=4;
 
-        if ((one > two)&& (one > three)){
-            System.out.println("Самое большое число -"+ one);
-        } else if (one < two && (two>= three)) {
-            System.out.println("Cамое большое число -" +two);
-        } else {
-            System.out.println("Самое большое число -" + three);
+    {
+        int clientOS = 0;
+        int clientDeviceYear = 2015;
+
+        if (clientDeviceYear <= 2015) {
+
+        switch (clientOS) {
+            case 0:
+                System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+                break;
+            case 1:
+                System.out.println("Установите облегченную версию приложения для Android по ссылке");
+                break;
+        }
+    } else{
+        switch (clientOS) {
+            case 0:
+                System.out.println("Установите версию приложения для iOS по ссылке");
+                break;
+            case 1:
+                System.out.println("Установите версию приложения для Android по ссылке");
+                break;
         }
     }
     }
+    {
+        int year = 2021;
+
+        if (year <= 1584) {
+            System.out.println("Год должен быть больше 1584.");
+        } else {
+            boolean isLeap = (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
+
+            if (isLeap) {
+                System.out.println(year + " год является високосным");
+            } else {
+                System.out.println(year + " год не является високосным");
+            }
+
+    }
+}
+    {
+        int deliveryDistance = 95;
+
+        if (deliveryDistance > 100) {
+            System.out.println("Доставки нет");
+            if (deliveryDistance >= 60){
+                System.out.println("Потребуется 3 дня доставки");
+            } else if (deliveryDistance >= 20) {
+                 System.out.println("Потребуется 2 дня для доставки");
+            }else{
+                System.out.println("Потребуется 1 день для доставки");
+
+        }
+    }
+    }
+    {
+        int monthNumber = 12;
+
+        switch (monthNumber){
+                case 1:
+                System.out.println("Январь");
+                    break;
+                    case 2:
+                    System.out.println("Февраль");
+                    break;
+                case 3:
+                    System.out.println("Март");
+                    break;
+                case 4:
+                    System.out.println("Апрель");
+                    break;
+                case 5:
+                    System.out.println("Май");
+                    break;
+                case 6:
+                    System.out.println("Июнь");
+                    break;
+                case 7:
+                    System.out.println("Июль");
+                    break;
+                case 8:
+                    System.out.println("Август");
+                    break;
+                case 9:
+                    System.out.println("Сентябрь");
+                    break;
+                case 10:
+                    System.out.println("Октябрь");
+                    break;
+                case 11:
+                    System.out.println("Ноябрь");
+                    break;
+                case 12:
+                    System.out.println("Декабрь");
+                    break;
+            default:
+                System.out.println("В году всего 12 месяцев");
+            }
+    }
+}
