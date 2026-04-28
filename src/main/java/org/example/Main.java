cdc2b909a13a1d0335216f95a75822889c7f947b
Комочек <asasinre@mail.ru>
2026-04-28 18:11:33 +0300

    homework_5

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -37,21 +37,21 @@ public class Main {
             }
         }
     }
+
     {
         int year = 2021;
 
         if (year <= 1584) {
-            System.out.println("Год должен быть больше 1584.");
+            System.out.println("Год должен быть больше, чем 1584.");
         } else {
-            boolean isLeap = (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
-
-            if (isLeap) {
+            if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
                 System.out.println(year + " год является високосным");
             } else {
                 System.out.println(year + " год не является високосным");
             }
         }
     }
+
     {
         int deliveryDistance = 95;
         int deliveryDays = 0;
@@ -70,10 +70,11 @@ public class Main {
             System.out.println("Потребуется дней: " + deliveryDays);
         }
     }
+
     {
         int monthNumber = 12;
 
-        switch (monthNumber){
+        switch (monthNumber) {
             case 1:
             case 2:
             case 12:
