d83c2ca7c71ccc753c65c02da0edeeb1aa2b2854
Комочек <asasinre@mail.ru>
2026-05-18 07:19:39 +0300

    homework_7

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,7 @@
 package org.example;
 import java.util.Arrays;
 public class Main {
-    {
+    public static void main(String[] args) {
         int[] arr = new int[]{1, 2, 3};
         double[] arr1 = new double[]{1.57d, 7.654d, 9.986d};
         int[] arr2 = new int[]{7, 9, 0, 4, 5};
@@ -28,17 +28,17 @@ public class Main {
         }
 
         for (int i = arr2.length - 1; i >= 0; i--) {
-            output.append(arr2[i]);
             if (i > 0) {
-                output.append(", ");
+                System.out.print(arr2[i] + ", ");
+            } else {
+                System.out.println(arr2[i]);
             }
         }
-        System.out.println(output);
 
 
         int i=0;
 
-        for (i = 0, i < arr.length, i++) {
+        for (i = 0; i < arr.length; i++) {
             if (arr[i] % 2 != 0) {
                 arr[i] = arr[i] + 1;
             }
