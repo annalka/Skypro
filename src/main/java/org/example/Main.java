51ad4d578dadce9255c1cc16f89056e43b74cb27
Комочек <asasinre@mail.ru>
2026-06-02 08:43:07 +0300

    homework_13

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,50 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        System.out.println("=== ТЕСТИРОВАНИЕ ТОВАРОВ ===");
 
-        for (int i = 1; i <= 7; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        Product p1 = new Product(101, "Ноутбук", 50000, "Электроника");
+        Product p2 = new Product(102, "Мышь", 1500, "Электроника");
+        Product p3 = new Product(101, "Другой ноутбук", 60000, "Электроника");
+        Product p4 = new Product(103, "Книга", 500, "Литература");
+
+        System.out.println("Товары:");
+        System.out.println(p1);
+        System.out.println(p2);
+        System.out.println(p3);
+        System.out.println(p4);
+
+        System.out.println("\nРезультаты сравнения товаров:");
+        System.out.println("p1 == p2: " + p1.equals(p2)); // false
+        System.out.println("p1 == p3: " + p1.equals(p3)); // true (совпадают id и категория)
+        System.out.println("p1 == p4: " + p1.equals(p4)); // false
+
+        // Тестирование класса Order
+        System.out.println("\n=== ТЕСТИРОВАНИЕ ЗАКАЗОВ ===");
+
+        Product[] basket1 = {p1, p2};
+        Product[] basket2 = {p1, p2};
+        Product[] basket3 = {p2, p1}; // другой порядок
+        Product[] basket4 = {p1, p2, p4}; // дополнительный товар
+
+        Order order1 = new Order("Иван Иванов", basket1);
+        Order order2 = new Order("Иван Иванов", basket2);
+        Order order3 = new Order("Пётр Петров", basket1); // другой покупатель
+        Order order4 = new Order("Иван Иванов", basket3);
+        Order order5 = new Order("Иван Иванов", basket4);
+
+        System.out.println("Заказы:");
+        System.out.println(order1);
+        System.out.println(order2);
+        System.out.println(order3);
+        System.out.println(order4);
+        System.out.println(order5);
+
+        System.out.println("\nРезультаты сравнения заказов:");
+        System.out.println("order1 == order2: " + order1.equals(order2)); // true
+        System.out.println("order1 == order3: " + order1.equals(order3)); // false (другой покупатель)
+        System.out.println("order1 == order4: " + order1.equals(order4)); // false (другой порядок)
+        System.out.println("order1 == order5: " + order1.equals(order5)); // false (разные корзины)
     }
 }
---
+++ src/main/java/org/example/Order.java
@@ -0,0 +1,66 @@
+package org.example;
+
+import java.util.Arrays;
+import java.util.Objects;
+
+public class Order {
+    private String customer;
+    private Product[] basket;
+
+    public Order(String customer, Product[] basket) {
+        this.customer = customer;
+        this.basket = basket;
+    }
+
+    public String getCustomer() {
+        return customer;
+    }
+
+    public Product[] getBasket() {
+        return basket;
+    }
+
+    @Override
+    public String toString() {
+        return "Заказ[покупатель=" + customer +
+                ", корзина=" + Arrays.toString(basket) + "]";
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+
+        Order order = (Order) obj;
+
+        if (!Objects.equals(customer, order.customer)) return false;
+
+        Product[] thisBasket = this.basket;
+        Product[] otherBasket = order.basket;
+
+        if (thisBasket == null && otherBasket == null) return true;
+        if (thisBasket == null || otherBasket == null) return false;
+
+        if (thisBasket.length != otherBasket.length) return false;
+
+        for (int i = 0; i < thisBasket.length; i++) {
+            Product thisProduct = thisBasket[i];
+            Product otherProduct = otherBasket[i];
+
+            if (thisProduct == null && otherProduct == null) continue;
+            if (thisProduct == null || otherProduct == null) return false;
+
+            if (!thisProduct.equals(otherProduct)) return false;
+        }
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = Objects.hash(customer);
+        result = 31 * result + Arrays.hashCode(basket);
+        return result;
+    }
+}
+
