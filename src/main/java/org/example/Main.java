2870eb54f3fcc04970d938179bad4c06b1c61c8a
Комочек <asasinre@mail.ru>
2026-06-02 08:21:13 +0300

    homework_11

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,24 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        public static void main(String[] args) {
 
-        for (int i = 1; i <= 7; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+            Author author1 = new Author("Лев", "Толстой");
+            Author author2 = new Author("Фёдор", "Достоевский");
+
+            Book book1 = new Book("Война и мир", author1, 1869);
+            Book book2 = new Book("Преступление и наказание", author2, 1866);
+
+            System.out.println("До изменения года публикации:");
+            System.out.println(book1.getTitle() + " (" + book1.getAuthor().getFirstName() +
+                    " " + book1.getAuthor().getLastName() + ") — " + book1.getPublicationYear());
+            System.out.println(book2.getTitle() + " (" + book2.getAuthor().getFirstName() +
+                    " " + book2.getAuthor().getLastName() + ") — " + book2.getPublicationYear());
+
+            book1.setPublicationYear(1870);
+
+            System.out.println("\nПосле изменения года публикации:");
+            System.out.println(book1.getTitle() + " (" + book1.getAuthor().getFirstName() +
+                    " " + book1.getAuthor().getLastName() + ") — " + book1.getPublicationYear());
         }
     }
-}
